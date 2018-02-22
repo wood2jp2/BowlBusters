91f284a6eb6e5d9af15833d0618e3ec2d6c72cf7
Josh Wood <wood2jp2@gmail.com>
2018-02-21 20:38:56 -0500

    ten frames mapping out to page

--- src/Components/Frames.js
+++ src/Components/Frames.js
@@ -6,15 +6,39 @@ class Frames extends Component {
     super()
     this.state = {
       bonusFrame: false,
-      frames: [],
-      frameCount: 1
+      scores: [[], [], [], [], [], [], [], [], [], []],
+      frameCount: 10
     }
   }
 
-  render() {
-    return <p>{this.state.something}</p>
+  addScores = e => {
+    e.preventDefault()
+
   }
 
+  render() {
+
+    let output = this.state.scores.map( (x, i) => {
+      return (
+        <div key={i}>
+          <p>Frame {i+1}</p>
+          <input
+            name='roll'
+            value={this.state.scores[i][0]}
+            onChange={ e => this.addScores(e)}>
+            </input>
+          <input
+            name='roll'
+            value={this.state.scores[i][1]}
+            onChange={ e => this.addScores(e)}>
+            </input>
+        </div>
+      )
+    })
+
+    return output
+
+  }
 }
 
 export default Frames
