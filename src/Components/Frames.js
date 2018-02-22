769e2ed8c4fb77d591aa7e4e7b9d50347762c403
Josh Wood <wood2jp2@gmail.com>
2018-02-21 22:08:05 -0500

    all ten frames correctly changing state

--- src/Components/Frames.js
+++ src/Components/Frames.js
@@ -6,31 +6,41 @@ class Frames extends Component {
     super()
     this.state = {
       bonusFrame: false,
-      scores: [[], [], [], [], [], [], [], [], [], []],
+      scores: [['12', '21'], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['', ''], ['','']],
+      frames: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       frameCount: 10
     }
   }
 
-  addScores = e => {
+  addScores = (e, i) => {
     e.preventDefault()
+    let currentFrame = this.state.scores[e.target.name]
+    currentFrame[i-1] = e.target.value
+    let replaceFrameInState = this.state.scores.splice(e.target.name, 1, currentFrame)
+    // this.setState({
+    //   scores: e.target.value
+    // })
 
+    console.log(this.state.scores)
   }
 
   render() {
 
-    let output = this.state.scores.map( (x, i) => {
+    let output = this.state.frames.map( (x, i) => {
       return (
-        <div key={i}>
+        <div id={i} key={i}>
           <p>Frame {i+1}</p>
           <input
-            name='roll'
-            value={this.state.scores[i][0]}
-            onChange={ e => this.addScores(e)}>
+            name={i}
+            maxLength={1}
+            ref={1}
+            onChange={ e => this.addScores(e, 1)}>
             </input>
           <input
-            name='roll'
-            value={this.state.scores[i][1]}
-            onChange={ e => this.addScores(e)}>
+            name={i}
+            maxLength={1}
+            ref={2}
+            onChange={ e => this.addScores(e, 2)}>
             </input>
         </div>
       )
