18c3c3b73f898d156bdd6fabdcfaee034308ada3
Josh Wood <wood2jp2@gmail.com>
2018-02-25 23:51:54 -0500

    decided upon the styled components package

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import './App.css'
 import Header from './Components/Header'
 import Game from './Components/Game'
 import Footer from './Components/Footer'
-import sc from 'styled-components'
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+  background-color: #E87DE4;
+  text-align: center;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrap className="App">
         <header className="App-header">
           <Header />
 
-          <h1 className="App-title">Enjoy your own bowling scorer on your way to 300!</h1>
+
         </header>
         <Game />
         <Footer />
-      </div>
+      </Wrap>
     )
   }
 }
